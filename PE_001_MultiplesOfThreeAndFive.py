228cbca585139bdce4e382d36a1fa0d6930f4930
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2020-12-31 13:48:00 -0300

    problem 1 done!

--- PE_001_MultiplesOfThreeAndFive.py
+++ PE_001_MultiplesOfThreeAndFive.py
@@ -1,4 +1,4 @@
-from time import time
+from PE_000_Utils import timed
 
 # == Project Euler: Problem 1 ==================================================================== #
 """
@@ -9,8 +9,8 @@ The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 be
 # == Solution 1 ================================================================================== #
 
 
+@timed
 def solution_1(upper_bound=1000):
-    time_elapsed = time()
     total = 0
 
     for number in range(3, upper_bound):
@@ -18,15 +18,12 @@ def solution_1(upper_bound=1000):
         if number % 3 == 0 or number % 5 == 0:
             total += number
 
-    time_elapsed = (time() - time_elapsed) * 1000
-
-    print(f'The answer is: {total}')
-    print(f'That took {time_elapsed}ms')
-
+    return total
 
 # == Solution 2 ================================================================================== #
 
 
+@timed
 def solution_2(upper_bound=1000):
     """
     Solution 1 refactored
@@ -35,17 +32,16 @@ def solution_2(upper_bound=1000):
     :return:
     """
 
-    time_elapsed = time()
-    total = sum(filter(lambda number: not number % 3 or not number % 5, range(3, upper_bound)))
-    time_elapsed = (time() - time_elapsed) * 1000
-
-    print(f'The answer is: {total}')
-    print(f'That took {time_elapsed}ms')
+    return sum(filter(lambda number: not number % 3 or not number % 5, range(3, upper_bound)))
 
 
 if __name__ == '__main__':
-    print('Solution 1')
+    print('Project Euler: Multiples of Three and Five')
+
+    print()
+    print('-> Solution 1')
     solution_1()
 
-    print('Solution 2')
+    print()
+    print('-> Solution 2')
     solution_2()
