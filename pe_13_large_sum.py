74fa64a2ab4f9e0c03fdf697789724c93c40827f
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-01-31 22:13:50 -0300

    done!

---
+++ pe_13_large_sum.py
@@ -0,0 +1,19 @@
+from pe_000_utils import print_answers, timed
+
+# == Project Euler: Problem 13 ======================================================================================= #
+"""
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+"""
+
+# == Solution 1 ====================================================================================================== #
+
+
+@timed
+def solution_1(path: str = 'pe_13_large_sum.txt') -> int:
+    
+
+    return 0
+
+
+if __name__ == '__name__':
+    print_answers('Large Sum', solution_1)
