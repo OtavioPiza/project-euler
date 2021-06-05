c2aa7ed20948e7d793446b7dabb477b3f67b469f
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-05 16:46:58 -0300

    further improved decorator

--- timing.py
+++ timing.py
@@ -1,27 +1,44 @@
-from typing import Tuple, Any, Dict
 from functools import wraps
+from statistics import mean
 from time import perf_counter
+from typing import Tuple, Any, Dict, List
 
 
-def timed(function: (Tuple[Any, ...], Dict[str, Any])) -> (Tuple[Any, ...], Dict[str, Any]):
-    """
-    This decorator serves to the execution time of the function it decorates returning a tuple with (result, time)
-
-    :param function: function to be decorated
-    :return: function
+def timed(runs: int) -> int:
     """
 
-    @wraps(function)
-    def run(*args, **kwargs):
-        """
-        wrapper
+    :param runs: how many times the function should be ran
+    :return:
+    """
 
-        :param args:
-        :param kwargs:
-        :return: (function result, time elapsed)
+    def decorator(function: (Tuple[Any, ...], Dict[str, Any])) -> (Tuple[Any, ...], Dict[str, Any]):
         """
-        elapsed = perf_counter()
-        answer = function(*args, **kwargs)
-        return answer, perf_counter() - elapsed
+        This decorator serves to the execution time of the function it decorates returning a tuple with (result, time)
 
-    return run
+        :param function: function to be decorated
+        :return: function
+        """
+
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            """
+            wrapper
+
+            :param args:
+            :param kwargs:
+            :return: (function result, time elapsed)
+            """
+            times: List[float, ...] = []
+            answer: Any = None
+
+            for _ in range(runs):
+                elapsed = perf_counter()
+                answer = function(*args, **kwargs)
+                times.append(perf_counter() - elapsed)
+
+            return answer, mean(times)
+
+        return wrapper
+
+    return decorator
+
