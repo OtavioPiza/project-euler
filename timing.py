a7a4c864e0cf5cae0c9a15197b7799588621ecc1
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-05 16:54:18 -0300

    further improved decorator

--- timing.py
+++ timing.py
@@ -4,8 +4,9 @@ from time import perf_counter
 from typing import Tuple, Any, Dict, List
 
 
-def timed(runs: int) -> int:
+def timed(runs: int):
     """
+    This decorator serves to the execution time of the function it decorates returning a tuple with (result, time)
 
     :param runs: how many times the function should be ran
     :return:
@@ -13,19 +14,18 @@ def timed(runs: int) -> int:
 
     def decorator(function: (Tuple[Any, ...], Dict[str, Any])) -> (Tuple[Any, ...], Dict[str, Any]):
         """
-        This decorator serves to the execution time of the function it decorates returning a tuple with (result, time)
 
         :param function: function to be decorated
         :return: function
         """
 
         @wraps(function)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Tuple[Any], **kwargs: Dict[str, Any]) -> Tuple[Any, float]:
             """
             wrapper
 
-            :param args:
-            :param kwargs:
+            :param args: args
+            :param kwargs: kwargs
             :return: (function result, time elapsed)
             """
             times: List[float, ...] = []
