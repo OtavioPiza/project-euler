5124dccb2403987174bf1e3c2f205da369f68f17
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-10 16:52:37 -0300

    added docs

--- utils/plotting.py
+++ utils/plotting.py
@@ -3,6 +3,13 @@ from typing import Any, NoReturn, Tuple, List
 
 
 def plot_range(params: Tuple[Tuple[Any], ...], functions: Tuple[(Any, )]) -> NoReturn:
+    """
+    plots the time each function took to execute each of the provided parameters; if there are more then one parameters,
+    the first one is used for the x-axis
+
+    :param params: parameters for the functions
+    :param functions: functions
+    """
     x_axis: List[Any, ...] = list(map(lambda i: i[0], params))
     y_axes: List[List[float, ...]] = [[function(*param)[1] for param in params] for function in functions]
     index: int = 0
