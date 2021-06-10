d560a87d61a5dcf2a63d42ea0c037857e1cc8171
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-10 15:51:39 -0300

    created plotting tool)

---
+++ utils/plotting.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+from typing import Any, NoReturn, Tuple, List
+
+
+def plot_range(params: Tuple[Tuple[Any], ...], functions: Tuple[(Any, )]) -> NoReturn:
+    x_axis: List[Any, ...] = list(map(lambda i: i[0], params))
+    y_axes: List[List[float, ...]] = [[function(*param)[0] for param in params] for function in functions]
+    index: int = 0
+
+    for y_axis in y_axes:
+        plt.plot(x_axis, y_axis, label=f'line {(index := index + 1)}')
+
+    plt.legend()
+    plt.show()
