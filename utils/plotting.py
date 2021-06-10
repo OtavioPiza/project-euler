f81a8151b3bfa249be93e69fb33cd1b4a9abc142
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-10 16:22:02 -0300

    removed debugging prints

--- utils/plotting.py
+++ utils/plotting.py
@@ -6,7 +6,6 @@ from concurrent.futures import ThreadPoolExecutor
 def plot_range(params: Tuple[Tuple[Any], ...], functions: Tuple[(Any, )]) -> NoReturn:
     x_axis: List[Any, ...] = list(map(lambda i: i[0], params))
     y_axes: List[List[float, ...]] = [[function(*param)[1] for param in params] for function in functions]
-    print(y_axes)
     index: int = 0
 
     for y_axis in y_axes:
