bcf7c0aaad370803de88cf894d914bb75396d041
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-10 16:21:42 -0300

    wip

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,10 +1,12 @@
 from matplotlib import pyplot as plt
 from typing import Any, NoReturn, Tuple, List
+from concurrent.futures import ThreadPoolExecutor
 
 
 def plot_range(params: Tuple[Tuple[Any], ...], functions: Tuple[(Any, )]) -> NoReturn:
     x_axis: List[Any, ...] = list(map(lambda i: i[0], params))
-    y_axes: List[List[float, ...]] = [[function(*param)[0] for param in params] for function in functions]
+    y_axes: List[List[float, ...]] = [[function(*param)[1] for param in params] for function in functions]
+    print(y_axes)
     index: int = 0
 
     for y_axis in y_axes:
